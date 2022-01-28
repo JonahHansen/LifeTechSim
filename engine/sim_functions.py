4025f27cf05136d8895c2063b0b2a2056f7bcf9c
JonahHansen <hansen.jonah833@gmail.com>
2022-01-28 19:09:07 +1100

    bug fixes

--- engine/sim_functions.py
+++ engine/sim_functions.py
@@ -79,8 +79,6 @@ def Planck_wrapper(T):
 #Limb darkening as a function of the fraction of stellar radius (1 = stellar radius)
 def limb_darkening(r):
     with np.errstate(divide='ignore', invalid='ignore'):
-        if r > 1:
-            r = 1
         mu = np.sqrt(1-r**2)
     return 1-0.05*(1-mu)-0.10*(1-mu)**2
 
