3cf87e717c3a25deb28a64c9fba0b3a0fe4942ce
JonahHansen <hansen.jonah833@gmail.com>
2022-01-28 17:46:49 +1100

    error_sim

---
+++ engine/nullers/five_telescopes_err.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+"""
+Calculates the positions of five telescopes in a regular pentagonal formation.
+
+Inputs:
+    baseline = the shorter baseline (distance between adjacent telescopes)
+Output:
+    List of x positions and list of y positions
+"""
+def pentagon(baseline):
+    R = 0.85065*baseline #To the radius of the pentagon
+    angles = np.linspace(0,2*np.pi,6) #Angular positions of the telescopes
+    #To cartesian
+    xs = R*np.cos(angles)
+    ys = R*np.sin(angles)
+    return np.array([xs,ys]).T[:-1]
+
+def calc_a(n,i,phi,theta):
+    a = np.identity(n,complex)
+
+    a[i,i] = np.sin(theta)
+    a[i,i+1] = np.exp(1j*phi)*np.cos(theta)
+    a[i+1,i] = np.cos(theta)
+    a[i+1,i+1] = -np.exp(1j*phi)*np.sin(theta)
+
+    return a
+
+def calc_K5_M(dphi,dR):
+
+    theta = np.array([7*np.pi/4,
+                     np.arcsin(1/np.sqrt(3)),
+                     np.arctan(np.sqrt(1/3*(4+np.sqrt(5)))),
+                     5*np.pi/6,
+                     -np.arcsin(1/3*np.sqrt(3+2/np.sqrt(5))),
+                     np.pi-np.arctan(np.sqrt(1/3*(4+np.sqrt(5)))),
+                     np.pi-np.arcsin(1/np.sqrt(5)),
+                     -np.pi/6,
+                     -np.arcsin(1/np.sqrt(3)),
+                     -np.pi/4])
+
+    phi = np.array([np.pi,
+                     np.pi,
+                     -np.pi/10+np.arctan(np.sqrt(5+2/np.sqrt(5))),
+                     np.pi,
+                     np.arctan(3/7*np.sqrt(5+2*np.sqrt(5))),
+                     np.pi-np.arctan(3*np.sqrt(1+2/np.sqrt(5))),
+                     np.pi,
+                     np.arctan(np.sqrt(2+2/np.sqrt(5))),
+                     -np.arctan(np.sqrt(5+2/np.sqrt(5))),
+                     9*np.pi/10])
+
+    dtheta = dR/np.cos(theta)
+
+    a1 = calc_a(5,3,phi[0]+dphi[0],theta[0]+dtheta[0])
+    a2 = calc_a(5,2,phi[1]+dphi[1],theta[1]+dtheta[1])
+    a3 = calc_a(5,3,phi[2]+dphi[2],theta[2]+dtheta[2])
+    a4 = calc_a(5,1,phi[3]+dphi[3],theta[3]+dtheta[3])
+    a5 = calc_a(5,2,phi[4]+dphi[4],theta[4]+dtheta[4])
+    a6 = calc_a(5,3,phi[5]+dphi[5],theta[5]+dtheta[5])
+    a7 = calc_a(5,0,phi[6]+dphi[6],theta[6]+dtheta[6])
+    a8 = calc_a(5,1,phi[7]+dphi[7],theta[7]+dtheta[7])
+    a9 = calc_a(5,2,phi[8]+dphi[8],theta[8]+dtheta[8])
+    a10 = calc_a(5,3,phi[9]+dphi[9],theta[9]+dtheta[9])
+
+    C = np.identity(5,complex)
+    C[4,4] = np.exp(1j*np.pi)
+
+    ls_mats = [C,a10,a9,a8,a7,a6,a5,a4,a3,a2,a1]
+
+    M = np.identity(5)
+    for ai in ls_mats:
+        M = np.matmul(M,ai)
+
+    #M*=np.sqrt(5)
+
+    return M
+
+"""
+Function to calculate the response map of a five telescope kernel nuller interferometer.
+
+Inputs:
+    baseline = length of the shorter baseline in meters (adjacent telescopes)
+    fov = total field of view of the interferometer in radians
+    sz = size of the array
+    base_wavelength = wavelength upon which the baseline is optimised. In meters
+
+Outputs:
+    List of modulation maps of the form:
+        (Transmission map, Kernel map)
+    The transmission map is one of the nulled outputs that is chosen to create the kernel.
+"""
+def get_nuller_response(dphi,dR,baseline,fov,sz,base_wavelength):
+
+    M = make_nuller_mat5(dphi,dR)
+
+    telescope_array = pentagon(baseline)
+
+    sky_angles = np.linspace(-fov/2,fov/2,sz)
+
+    xy = np.meshgrid(sky_angles, sky_angles, indexing='ij')
+
+    #x,y are telescope positions in units of wavelength
+    x = telescope_array[:,0]/base_wavelength
+    y = telescope_array[:,1]/base_wavelength
+
+    #Response is the 5 output electric fields as a function of the position on the sky
+    response = np.zeros((5,sz,sz), dtype='complex')
+
+    for i in range(5):
+        for k in range(5):
+            #Inputs have a phase equal to xy array - linear multiplied by spatial frequency
+            #ul + vm
+            response[k] += np.exp(2*np.pi*1j*(xy[0]*x[i] + xy[1]*y[i]))*M[k,i] #
+
+    response = np.abs(response)**2 #To intensity
+    response /= (np.max(response[0])/5) #normalise by flux per telescope
+
+    #This is turning the output intensities into the kernel nulls (K in 2018 paper)
+    k1 = response[1]-response[4]
+    k2 = response[2]-response[3]
+
+    return [(response[1],k1),(response[2],k2)] #return intensity per telescope
---
+++ error_sim.py
@@ -0,0 +1,190 @@
+import numpy as np
+import pickle
+from engine.sim_functions import calc_local_zodiacal_minimum,Spectrograph
+from engine.planet_retrieval import Planet,Star
+from engine.main_computer import compute
+from itertools import chain
+from multiprocessing import Pool
+import json
+import sys
+
+dR_scale = str(sys.argv[1])
+out_file = str(sys.argv[2])
+
+base_wave = 18
+#####################################################
+#Secondary parameters
+
+min_wave = 4 #microns
+max_wave = 19 #microns
+num_channels = 50
+
+#####################################################
+
+#Set up the spectral parameters
+spec = Spectrograph(min_wave,max_wave,base_wave,num_channels)
+
+from engine.nullers.five_telescopes_err import get_nuller_response
+    architecture_verbose = "Five telescope kernel nuller, optimised for diagonal telescopes (K2 alt)"
+    base_scale_factor = 1.028 #= approx 1.03*0.619 (where 0.619 is the conversion between a side and diagonal of a pentagon)
+
+
+sz = 1500
+mode_verbose = "Search"
+fov_scale_factor = 5
+
+dphi_scale = np.radians(dR_scale*180/np.pi)
+number_processes = 28 #parallelise?
+###########################################################################
+
+#Tau boo coordinates (ecliptic latitude of 26 degress)
+ra = 206.81560195
+dec = 17.45690446
+z = 1 #exozodi (same as solar system)
+
+#Generate stars of given types
+def Beta_Pic(dist):
+    name = "Beta Pic analogue"
+    stype = "A6V"
+    rad = 1.8
+    teff = 8052
+    mass = 1.75
+    return Star(name,1,dist,stype,rad,teff,mass,ra,dec,spec,z)
+
+def Tau_Boo(dist):
+    name = "Tau Boo analogue"
+    stype = "F7V"
+    rad = 1.42
+    teff = 6399
+    mass = 1.39
+    return Star(name,2,dist,stype,rad,teff,mass,ra,dec,spec,z)
+
+def Sun(dist):
+    name = "Solar analogue"
+    stype = "G2V"
+    rad = 1
+    teff = 5772
+    mass = 1
+    return Star(name,3,dist,stype,rad,teff,mass,ra,dec,spec,z)
+
+def Eps_Eri(dist):
+    name = "Epsilon Eri analogue"
+    stype = "K2V"
+    rad = 0.735
+    teff = 5084
+    mass = 0.82
+    return Star(name,4,dist,stype,rad,teff,mass,ra,dec,spec,z)
+
+def Prox_Cen(dist):
+    name = "Proxima Cen analogue"
+    stype = "M5V"
+    rad = 0.15
+    teff = 3042
+    mass = 0.12
+    return Star(name,5,dist,stype,rad,teff,mass,ra,dec,spec,z)
+
+#Helper function to generate Earth-twin planets
+def myPlanet(star,num,a):
+
+    #Earth twin
+    PRad = 1
+    PMass = 1
+    Temp = 300
+
+    Ageom = 0.1 #Rough estimate?
+    AngSep = a/star.Dist
+    lam_ref = 0.318 #From PPop, assuming face on orbit (inc = 0)
+
+    return Planet(star,0,star.SNumber,num,PRad,PMass,365,0,0,0,0,0,0,0,Ageom,a,a,AngSep,0,0,lam_ref,Temp,spec)
+
+#Give each star three planets aat the three different HZ places
+def append_planet_list(star):
+    star.Planets = [myPlanet(star,2,star.HZMid)]
+    return star
+
+dists = np.linspace(1,20,20)
+
+#Make the list of stars at given distances
+star_list = []
+for d in dists:
+    star_list.append(append_planet_list(Sun(d)))
+    star_list.append(append_planet_list(Eps_Eri(d)))
+    star_list.append(append_planet_list(Prox_Cen(d)))
+
+
+dphi = np.zeros(10)
+dR = np.zeros(10)
+
+dphi[2] = (np.random.random()*2-1)*dphi_scale
+dphi[4] = (np.random.random()*2-1)*dphi_scale
+dphi[5] = (np.random.random()*2-1)*dphi_scale
+dphi[7] = (np.random.random()*2-1)*dphi_scale
+dphi[8] = (np.random.random()*2-1)*dphi_scale
+dphi[9] = (np.random.random()*2-1)*dphi_scale
+
+dR[2] = (np.random.random()*2-1)*dR_scale
+dR[4] = (np.random.random()*2-1)*dR_scale
+dR[5] = (np.random.random()*2-1)*dR_scale
+dR[7] = (np.random.random()*2-1)*dR_scale
+dR[8] = (np.random.random()*2-1)*dR_scale
+dR[9] = (np.random.random()*2-1)*dR_scale
+
+def response_func(baseline,fov,sz,base_wavelength):
+    return get_nuller_response(dphi,dR,baseline,fov,sz,base_wavelength)
+
+
+
+###########################################################################
+
+#Get local zodi minimum
+local_exozodi = calc_local_zodiacal_minimum(spec)
+
+#RUN!!
+
+#Multiprocessing
+def worker_func(star):
+    return compute(star,1,response_func,spec,sz,base_scale_factor,fov_scale_factor,local_exozodi)
+
+pool = Pool(processes=number_processes)
+ls_star_data = pool.map(worker_func,star_list)
+pool.close()
+
+#Make into one list of dictionaries
+dict_ls = list(chain.from_iterable(ls_star_data))
+
+#Funtion to round sig figs (for numerical readibility)
+def round_sig_figs(x, p):
+    x_positive = np.where(np.isfinite(x) & (x != 0), np.abs(x), 10**(p-1))
+    mags = 10 ** (p - 1 - np.floor(np.log10(x_positive)))
+    return np.round(x * mags) / mags
+
+#Header data, plus results
+main_dict = {
+    "Architecture":architecture_verbose,
+    "Mode":mode_verbose,
+    "baseline_wavelength (microns)":round_sig_figs(spec.baseline_wave*1e6,5),
+    "sz":sz,
+    "fov_scale_factor":fov_scale_factor,
+    "min_wavelength (microns)":round_sig_figs(spec.wave_min*1e6,5),
+    "max_wavelength (microns)":round_sig_figs(spec.wave_max*1e6,5),
+    "num_channels":spec.n_channels,
+    "channel_widths (microns)":round_sig_figs(spec.dlambda*1e6,5),
+    "channel_centres (microns)":round_sig_figs(spec.channel_centres*1e6,5),
+    "results":dict_ls
+}
+
+#Needed for writing JSON
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+#Write file
+fout = open(out_file,"w")
+json.dump(main_dict,fout,cls=NpEncoder,indent=2)
+fout.close()
